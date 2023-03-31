5355cd68f8494c05354d49e1a835ecf7bc38c7b0
supercj <ukd163@163.com>
2023-03-31 15:13:03 +0800

    gitignore

--- damo/tti.py
+++ damo/tti.py
@@ -1,7 +1,7 @@
 from modelscope.utils.constant import Tasks
 from modelscope.pipelines import pipeline
 import cv2
-#文本转图片
+#文本转图片,text to image
 pipe = pipeline(task=Tasks.text_to_image_synthesis,
                 model='dienstag/vintedois-diffusion-v0-1',
                 model_revision='v0.1')
--- damo/tts.py
+++ damo/tts.py
@@ -1,7 +1,7 @@
 from modelscope.outputs import OutputKeys
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
-
+#text to speech
 text = '待合成文本'
 model_id = 'damo/speech_sambert-hifigan_tts_zhiyan_emo_zh-cn_16k'
 sambert_hifigan_tts = pipeline(task=Tasks.text_to_speech, model=model_id)
